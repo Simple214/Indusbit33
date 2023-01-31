1b16e1c05c9c65cb4c0eca7c5c08bf4feb9643c8
Simple212 <simplescalper@tutanota.com>
2023-01-31 13:45:41 +0530

    america_die22

--- src/App.js
+++ src/App.js
@@ -48,10 +48,11 @@ class App extends React.Component{
 	  bg_body:'#d4008e',
 	  display1:"block",
 	  display2:"none",
-	  top_color:"#ebc908",
+	  top_color:"#3166f0",
 	  url2:"./logo.png",
-	  row_bg:"#f0f2f5",
-	  row_color:"#d5018e"
+	  row_bg:"#ffffff",
+	  row_color:"#d5018e",
+	  bottom_color:'#d5018e'
     }
   }
 
@@ -938,7 +939,8 @@ bnbvalidation = (event2)=>{
 				top_color:"#2c3546",
 				url2:"./dark_logo.png",
 				row_bg:"#21262a",
-				row_color:"white"
+				row_color:"white",
+				bottom_color:'#000000'
 			})
 		}
 		
@@ -951,10 +953,11 @@ bnbvalidation = (event2)=>{
 				 bg_body:'#d4008e',
 				 display1:'block',
 				 display2:'none',
-				 top_color:"#ebc908",
+				 top_color:"#3166f0",
 				 url2:"./logo.png",
 				 row_bg:"#f0f2f5",
-				 row_color:"#d5018e"
+				 row_color:"#d5018e",
+				 bottom_color:'#d5018e'
 			})
 		}
 		
@@ -1049,7 +1052,7 @@ bnbvalidation = (event2)=>{
       <Main1 value10087={this.state.row_bg} value10088={this.state.row_color} logo_value={this.state.url2} value10085={this.state.top_color} value10081={this.state.bg_body} value10082={this.state.display1} value10083={this.state.display2} value10092={this.state.color1} value1008={this.state.text} value10091={this.state.url} changetodark={this.dark_theme} changetolight={this.light_theme} theme={this.state.theme} changetoetc1={this.changetoetc} changetoeth1={this.changetoeth}  changetosol1={this.changetosol} send2={this.setvalue} changetovet1={this.changetovet} changetoxtz1={this.changetoxtz} changetoada1={this.changetoada} changetotrx1={this.changetotrx} changetoavax1={this.changetoavax} first66={this.state.value876} first changetodash1={this.changetodash} changetobch1={this.changetobch13} changetobch2={this.changetobch26}  changetobnb1={this.changetobnb13}  changetobnb2={this.changetobnb14} changetoxmr2={this.changetoxmr2} change1={this.changevalue} value2 ={this.state.value2} value3={this.state.value3} value4={this.state.value4} value5={this.state.value5} value6={this.state.value6} reverse={this.reverse1} value10={this.state.value10} value11={this.state.value11} changetoxmr={this.changetoxmr1} changetodoge={this.changetodoge1} changetoltc={this.changetoltc1} changetobtc={this.changetobtc1}  changetodoge4={this.changetodoge2} changetoltc4={this.changetoltc2} changetobtc4={this.changetobtc2} event1={this.btcvalidation} value500={this.state.show} value1500={this.state.show1} value1600={this.state.show3} value501={this.state.show2} from1={'litecoin'} to1={'btc'} value74={'none'} value81={'block'} value75={'none'} value76={'block'} value71={'none'} value72={'none'} value73={'block'} value738 ={'block'} call={this.calltofinal} value1006={this.hide2} value10070={this.hide3} value739={'none'} backtohome={this.home234} value751={'none'} value741={'none'}/>
       
                           {/* Footer begins */}
-      <Row className='row1 d-flex justify-content-center align-items-center'>
+	<Row className='row1 d-flex justify-content-center align-items-center' style={{'background-color':`${this.state.bottom_color}`}}>
         <Col className='col-md-12 d-flex justify-content-center align-items-center'>
         <p className='b text-white text-center tc mt-4'>bitBarter © 2023</p>
         </Col>
@@ -1060,7 +1063,7 @@ bnbvalidation = (event2)=>{
           {this.state.from=='ltc' && this.state.to=='doge'
           ?<div>
      
-     <Main1 logo_value={this.state.url2} value10085={this.state.top_color} value10081={this.state.bg_body} value10082={this.state.display1} value10083={this.state.display2} value10092={this.state.color1} value1008={this.state.text} value10091={this.state.url} changetodark={this.dark_theme} changetolight={this.light_theme} theme={this.state.theme}  send2={this.setvalue} first66={this.state.value876} changetobch1={this.changetobch13} changetobch2={this.changetobch26} changetobnb1={this.changetobnb13}  changetobnb2={this.changetobnb14} change1={this.changevalue} changetoxmr2={this.changetoxmr2} value2 ={this.state.value2} value3={this.state.value3} value4={this.state.value4} value5={this.state.value5} value6={this.state.value6} reverse={this.reverse1} value10={this.state.value10} value1600={this.state.show3} value501={this.state.show2} value11={this.state.value11} changetodoge={this.changetodoge1} changetoxmr={this.changetoxmr1} changetoltc={this.changetoltc1} changetobtc={this.changetobtc120}  changetodoge4={this.changetodoge2} changetoltc4={this.changetoltc2} changetobtc4={this.changetobtc23} event1={this.dogevalidation} value500={this.state.show} value1500={this.state.show1}  from1={'litecoin'} to1={'doge'} value74={'block'} value75={'none'} value76={'none'} value71={'block'} value72={'none'} value73={'none'} value1006={this.hide2}  call={this.calltofinal} value738={'none'} value739={'none'} backtohome={this.home234} value760={'none'} value740={'none'} value750={'none'}  value751={'none'} value751={'none'} value741={'none'} />
+     <Main1 value10087={this.state.row_bg} value10088={this.state.row_color} logo_value={this.state.url2} value10085={this.state.top_color} value10081={this.state.bg_body} value10082={this.state.display1} value10083={this.state.display2} value10092={this.state.color1} value1008={this.state.text} value10091={this.state.url} changetodark={this.dark_theme} changetolight={this.light_theme} theme={this.state.theme}  send2={this.setvalue} first66={this.state.value876} changetobch1={this.changetobch13} changetobch2={this.changetobch26} changetobnb1={this.changetobnb13}  changetobnb2={this.changetobnb14} change1={this.changevalue} changetoxmr2={this.changetoxmr2} value2 ={this.state.value2} value3={this.state.value3} value4={this.state.value4} value5={this.state.value5} value6={this.state.value6} reverse={this.reverse1} value10={this.state.value10} value1600={this.state.show3} value501={this.state.show2} value11={this.state.value11} changetodoge={this.changetodoge1} changetoxmr={this.changetoxmr1} changetoltc={this.changetoltc1} changetobtc={this.changetobtc120}  changetodoge4={this.changetodoge2} changetoltc4={this.changetoltc2} changetobtc4={this.changetobtc23} event1={this.dogevalidation} value500={this.state.show} value1500={this.state.show1}  from1={'litecoin'} to1={'doge'} value74={'block'} value75={'none'} value76={'none'} value71={'block'} value72={'none'} value73={'none'} value1006={this.hide2}  call={this.calltofinal} value738={'none'} value739={'none'} backtohome={this.home234} value760={'none'} value740={'none'} value750={'none'}  value751={'none'} value751={'none'} value741={'none'} />
                                   {/* Footer begins */}
       <Row className='row1 d-flex justify-content-center align-items-center'>
         <Col className='col-md-12 d-flex justify-content-center align-items-center'>
