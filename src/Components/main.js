a25d56df457e8af62b6319ca361e933e8c98e5ce
Simple212 <simplescalper@tutanota.com>
2023-02-05 11:05:41 +0530

    en

--- src/Components/main.js
+++ src/Components/main.js
@@ -274,11 +274,11 @@ const Main1= ({reverse,value2,change1,value3,value4,value5,value6,value7,value10
                             </Dropdown.Toggle>
                             
                             <Dropdown.Menu style={{'background-color':`${value100910}`,'border':`${value10057}`}}>
-                            <Dropdown.Item style={{'color':`${value10059}`}} href="http://www.hi.bitbarter.org">हिंदी    <img src={require(`./india.jpg`)} className='flag1' width='30px' /> </Dropdown.Item>
-                            <Dropdown.Item style={{'color':`${value10059}`}} href="http://www.jp.bitbarter.org">日本語 <img src={require(`./japan.png`)} className='flag2' width='30px' /> </Dropdown.Item>
-                            <Dropdown.Item style={{'color':`${value10059}`}} href="http://www.cn.bitbarter.org">中文 <img src={require(`./china.png`)} className='flag3' width='30px' /> </Dropdown.Item>
-                            <Dropdown.Item style={{'color':`${value10059}`}} href="http://www.ru.bitbarter.org">руский <img src={require(`./russia.png`)} className='flag4' width='30px' /> </Dropdown.Item>
-                            <Dropdown.Item style={{'color':`${value10059}`}} href="http://www.bitbarter.org">English <img className='flag5' src={require(`./en.png`)} width='30px' /> </Dropdown.Item>
+                            <Dropdown.Item style={{'color':`${value10059}`}} href="https://www.hi.bitbarter.org" target="_blank">हिंदी    <img src={require(`./india.jpg`)} className='flag1' width='30px' /> </Dropdown.Item>
+                            <Dropdown.Item style={{'color':`${value10059}`}} href="https://www.jp.bitbarter.org" target="_blank">日本語 <img src={require(`./japan.png`)} className='flag2' width='30px' /> </Dropdown.Item>
+                            <Dropdown.Item style={{'color':`${value10059}`}} href="https://www.cn.bitbarter.org" target="_blank">中文 <img src={require(`./china.png`)} className='flag3' width='30px' /> </Dropdown.Item>
+                            <Dropdown.Item style={{'color':`${value10059}`}} href="https://www.ru.bitbarter.org" target="_blank">руский <img src={require(`./russia.png`)} className='flag4' width='30px' /> </Dropdown.Item>
+                            <Dropdown.Item style={{'color':`${value10059}`}} href="https://www.bitbarter.org" target="_blank">English <img className='flag5' src={require(`./en.png`)} width='30px' /> </Dropdown.Item>
                             </Dropdown.Menu>
                             
                             </Dropdown>
